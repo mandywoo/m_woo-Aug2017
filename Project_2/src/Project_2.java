867103f17d5a42092aaeb451ae9b795c95ebd44d
mandywoo <mandywoo10888@gmail.com>
2017-08-24 09:30:35 -0700

    Third time's a charm

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,15 @@
+//Mandy Woo
+/* August 24,2017
+This class compares output using print and println
+*/
+
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+
+	}
+
+}
