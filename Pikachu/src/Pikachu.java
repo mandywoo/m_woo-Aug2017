557f9da8ab5ec0f69f8be7c85061030fef5ca4f0
mandywoo <mandywoo10888@gmail.com>
2017-08-30 10:43:50 -0700

    Pikachu is working.

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,11 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("\t Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)");
+		System.out.println("\t\t\t  (o^.^)");
+		System.out.println("\t\t\t(_(\")(\")");
+	}
+
+}
