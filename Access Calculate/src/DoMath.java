922144c71b785b748161d3b3ac6a519eadecc14a
mandywoo <mandywoo10888@gmail.com>
2017-09-20 11:31:48 -0700

    Tested toMixedNum exception

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -12,7 +12,7 @@ public class DoMath {
 		System.out.println(Calculate.toDegrees(3.14159/2));
 		System.out.println(Calculate.toRadians(180));
 		System.out.println(Calculate.discriminant(1,1,1));
-		System.out.println(Calculate.toImproperFrac(3,1,2));
+		System.out.println(Calculate.toImproperFrac(-3,1,2));
 		System.out.println(Calculate.toMixedNum(7,2));
 		System.out.println(Calculate.foil(2,3,6,-7,"n"));
 		System.out.println(Calculate.isDivisibleBy(-14,7));
