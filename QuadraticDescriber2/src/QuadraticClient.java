6587f5c9b6b5bb2b3d4dc31b920ccfbd6f407d97
mandywoo <mandywoo@mandys-mbp>
2017-10-14 20:10:45 -0700

    Quadratic

--- QuadraticDescriber2/src/QuadraticClient.java
+++ QuadraticDescriber2/src/QuadraticClient.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
-
+/*Mandy Woo
+ * 
+ * Pseudocode
+ * QuadraticClient:
+ * Import Declaration before header
+ * Main Method
+ * Do, while loop(not quit)
+ * Initialize scanner
+ * Take in input values(a,b,c) for quadratic formula
+ * Put input values in quadrDesciber method
+ * Print output
+ * Prompt: Do you want to keep going? (Type "quit" to end)
+ * 
+ * Quadratic:
+ * main method
+ * quadrDescriber method
+ * standard form method
+ * direction it open: check if a is negative or positive
+ * axis of symmetry: complete the square
+ * vertex: complete the square
+ * x - intercept(s): quadForm method, then calculate 
+ * y- intercept: c
+ * add all to String
+ * return String
+ */
 public class QuadraticClient {
 
 	public static void main(String[] args) {
