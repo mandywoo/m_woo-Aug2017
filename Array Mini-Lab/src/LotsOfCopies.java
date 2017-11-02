1a86cc65c58fd5bccfa72e1d1f31ee73ae194ce3
mandywoo <mandywoo@mandys-mbp>
2017-11-01 21:42:06 -0700

    Finished Array mini-lab again

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //Mandy Woo
 public class LotsOfCopies {
 
@@ -6,16 +8,35 @@ public class LotsOfCopies {
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
 		changeMe(num, strMain, arrMain);
+		//num doesn't change
 		System.out.println(num);
+		//string doesn't change
 		System.out.println(strMain);
-		System.out.println(arrMain);
+		//prints changed to array
+		System.out.println(Arrays.toString(arrMain));
 
+		//intA changes, intB doesn't
+		int intA = 0;
+		int intB = intA;
+		intA = 1;
+		System.out.println(intA + " " + intB);
+		
+		//strA changes, strB doesn't
+		String strA = "Hello";
+		String strB = strA;
+		strA = "World";
+		System.out.println(strA + " " + strB);
+		
+		//both arrA and arrB change
+		int[] arrA = {1};
+		int[] arrB = arrA;
+		arrA[0] = 4;
+		System.out.println(Arrays.toString(arrA) + " " + Arrays.toString(arrB));
 	}
 	public static void changeMe(int x, String str, int[] arr) {
 		x += 1;
 		str += "2";
 		arr[3]++;
 	}
-	
 }
 
